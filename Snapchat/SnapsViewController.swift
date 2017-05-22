4c9890e8d1988a5ee58d64eda59baee84d1007bd
Victor Dill'Erva <victor.dillerva@tecsup.edu.pe>
2017-05-22 10:28:06 -0500

    hasta el momento se puede iniciar sesion, registrar un usuario en Firebase y se puede seleccionar una foto del carrete

---
+++ Snapchat/SnapsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SnapsViewController.swift
+//  
+//
+//  Created by DillerVaster on 21/05/17.
+//
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
